0152031c67494b297f080159280f866c971a93ef
vladi227 <vladimorales62@gmail.com>
2021-09-02 18:40:47 -0300

    cambios en nombre de musica.html

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.use(express.static('public'))
 
 app.get('/',(rep,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
 
+app.get('/about.html',(rep,res)=>res.sendFile(path.join(__dirname,'views','about.html')))
+
+app.get('/musica.html',(rep,res)=>res.sendFile(path.join(__dirname,'views','musica.html')))
+
+app.get('/conctact.html',(rep,res)=>res.sendFile(path.join(__dirname,'views','conctact.html')))
 app.listen(port,()=> console.log('serveidor funcionando en el puerto' + port))
