f951c75583736a303bb690406fc5f02931b19dda
vladi227 <vladimorales62@gmail.com>
2021-09-02 16:42:49 -0300

    inicio de emilia web

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/',(rep,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+
+app.listen(port,()=> console.log('serveidor funcionando en el puerto' + port))
